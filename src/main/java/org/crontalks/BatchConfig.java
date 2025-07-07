68d33e88ff12f4125c506bf5e020233a23919418
L <eleaz.rs@gmail.com>
2025-07-07 20:32:56 +0200

    add H2 db, and first batch skeleton

---
+++ src/main/java/org/crontalks/BatchConfig.java
@@ -0,0 +1,44 @@
+package org.crontalks;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+
+    @Bean
+    public Job reminderJob(JobRepository jobRepository, Step step1) {
+        return new JobBuilder("reminderJob", jobRepository)
+            .start(step1)
+            .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+            .tasklet((_, _) -> {
+                System.out.println(">>> Executing step1 from job...");
+                return RepeatStatus.FINISHED;
+            }, transactionManager)
+            .build();
+    }
+
+    @Bean
+    public JobRepository jobRepository() {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDatabaseType("H2");
+        try {
+            return factory.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/crontalks/ReminderScheduler.java
@@ -0,0 +1,29 @@
+package org.crontalks;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReminderScheduler {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job reminderJob;
+
+    @Scheduled(cron = "30 31 20 ? * MON")
+    public void runReminderJob() throws Exception {
+        JobParameters params = new JobParametersBuilder()
+            .addLong("run.id", System.currentTimeMillis()) // parámetro único
+            .toJobParameters();
+
+        System.out.println(">>> Lanzando reminderJob programado...");
+        jobLauncher.run(reminderJob, params);
+    }
+
+}
