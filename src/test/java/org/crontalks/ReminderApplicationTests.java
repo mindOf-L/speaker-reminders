be26a2076a03f47185e0cab0ca337135c8d893bb
L <eleaz.rs@gmail.com>
2025-09-08 21:15:49 +0200

    fix tests

--- src/test/java/org/crontalks/ReminderApplicationTests.java
+++ src/test/java/org/crontalks/ReminderApplicationTests.java
@@ -2,12 +2,13 @@ package org.crontalks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.yml")
 class ReminderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() { }
 
 }
