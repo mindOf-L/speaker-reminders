c96efa965c0ee6e7b1abdc587830e98d0a158f75
L <eleaz.rs@gmail.com>
2026-02-02 12:05:50 +0100

    update to spring boot 4

--- src/main/java/org/crontalks/config/BatchConfig.java
+++ src/main/java/org/crontalks/config/BatchConfig.java
@@ -1,18 +1,27 @@
 package org.crontalks.config;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BatchConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSource dataSource = new DriverManagerDataSource();
+        return new DataSourceTransactionManager(dataSource);
+    }
 
     @Bean
     public Job reminderJob(JobRepository jobRepository, Step step1) {
@@ -24,21 +33,10 @@ public class BatchConfig {
     @Bean
     public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step1", jobRepository)
-            .tasklet((_, _) -> {
+            .tasklet((contribution, chunkContext) -> {
                 System.out.println(">>> Executing step1 from job...");
                 return RepeatStatus.FINISHED;
             }, transactionManager)
             .build();
-    }
-
-    @Bean
-    public JobRepository jobRepository() {
-        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
-        factory.setDatabaseType("H2");
-        try {
-            return factory.getObject();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
